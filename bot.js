05d4935091685369f7faa634ea0d0e628237d832
AMelton75 <35177730+AMelton75@users.noreply.github.com>
2018-11-06 17:52:50 -0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -5,7 +5,7 @@ var i = 0;
 while (i<1) {
 console.log('Hello world');
 ajax({
-    url: 'https://hooks.slack.com/services/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',
+    url: "https://hooks.slack.com/services/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",
     type: 'POST',
     dataType: 'json',
     data: 'payload=' + JSON.stringify({
