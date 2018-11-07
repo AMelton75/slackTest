93fdcdbad75f826aa9f373d425ba9a7a83bc630b
Bin <jpahman2003@gmail.com>
2018-11-07 08:41:20 -0600

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,4 +1,4 @@
-var ajax = require('ajax');
+var xhttp = new XMLHttpRequest();
 var url = "https://hooks.slack.com/services/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";
 var text = "javascript test"
 var i = 0;
