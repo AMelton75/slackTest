ec4e8aaedb2987515f77d2525b89965759d915ae
Bin <jpahman2003@gmail.com>
2018-11-07 08:35:00 -0600

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,4 +1,5 @@
 var ajax = require('node.ajax');
+var $ = require('jQuery');
 var url = "https://hooks.slack.com/services/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";
 var text = "javascript test"
 var i = 0;
