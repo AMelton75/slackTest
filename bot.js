0cd66dfdc2bcdfbac1d863ca931ab692cc507439
Bin <jpahman2003@gmail.com>
2018-11-07 08:39:35 -0600

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,19 +1,10 @@
-var ajax = require('node.ajax');
-var $ = require('jquery');
 var url = "https://hooks.slack.com/services/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";
 var text = "javascript test"
 var i = 0;
 while (i<1) {
 console.log('Hello world');
-$.ajax({
-    url: url,
-    type: 'POST',
-    data: JSON.stringify({
-        "text": text
-    }),
-    contentType: "application/json",
-    complete: function() {console.log('completed request successfully');}
-    //processData: false,
-});
+xhttp.open("POST", url, true);
+xhttp.setRequestHeader("Content-type", "application/json");
+xhttp.send(JSON.stringify({text: text}));
     i++;
 }
