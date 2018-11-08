0805214e17e248b3b8dc7dfcfaf862deeab986f0
AMelton75 <35177730+AMelton75@users.noreply.github.com>
2018-11-08 10:29:22 -0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,7 +1,7 @@
 var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
 var xhttp = new XMLHttpRequest();
 var url = "https://hooks.slack.com/services/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";
-var text = "javascript test"
+var text = "Hey there Rohan"
 var i = 0;
 while (i<1) {
 console.log('Hello world');
