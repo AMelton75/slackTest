4d244a0a81f52cc834ea796ceec3a5596108f491
AMelton75 <35177730+AMelton75@users.noreply.github.com>
2018-11-08 10:36:10 -0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -3,10 +3,13 @@ var xhttp = new XMLHttpRequest();
 var url = "https://hooks.slack.com/services/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";
 var text = "Hey there Rohan"
 var i = 0;
+var j;
 if (i == 0) {
-console.log('Hello world');
-xhttp.open("POST", url, true);
-xhttp.setRequestHeader("Content-type", "application/json");
-xhttp.send(JSON.stringify({text: text}));
+    for (j = 0; j < 3; j++) {
+        console.log('Hello world');
+        xhttp.open("POST", url, true);
+        xhttp.setRequestHeader("Content-type", "application/json");
+        xhttp.send(JSON.stringify({text: text + j}));
+    }
     i++;
 }
