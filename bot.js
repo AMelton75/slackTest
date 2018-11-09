8d24911b2feec902aec2303dd69bfb9724343851
AMelton75 <35177730+AMelton75@users.noreply.github.com>
2018-11-09 11:03:33 -0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,19 +1,37 @@
 var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
 var xhttp = new XMLHttpRequest();
 var url = "https://hooks.slack.com/services/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";
-var text = "<@U8REZ2U3U>"
+var joshP = "<@U8REZ2U3U>";
+var rohan = "<@U0KFF3WDS>";
+var austin = "<@U7529SGN4>";
 var i = 0;
 var j;
-var json = JSON.stringify({
+var json = JSON.stringify(
+{
+  "mrkdwn": false,
+  "text": "*This will not be bold.* It will be surrounded gloriously with stars."
+  "mrkdwn": true,
+  "text": "*This will not be bold.* It will be surrounded gloriously with stars."
   "attachments": [
-        {
-            "title": "Title",
-            "pretext": "Pretext _supports_ mrkdwn",
-            "text": "Testing *right now!*",
-            "mrkdwn_in": ["text", "pretext"]
-        }
+  {
+  "pretext": joshP + " has the big gay",
+  "title": rohan + ":" + austin + " has a quest for you",
+  "text": "_You_",
+    "attachments": [
+    {
+    "text": "_Must Find_"
+      "attachments": [
+      {
+      "text": "*A Shrubbery!*"
+      }
+      ]
+    }
     ]
-                          });
+  }
+  ],
+  "text": "One that is nice but not too expensive"
+}
+);
 if (i == 0) {
     for (j = 0; j < 1; j++) {
         console.log('Hello world');
